867bb9cc4db325cfead04a0400f723e1320acad6
Rodrigo Bazán <bazanzuritarodrigo@gmail.com>
2021-06-01 00:29:54 -0500

    Eliminados comentarios

--- Scrapper.py
+++ Scrapper.py
@@ -1,7 +1,3 @@
-'''
-Este código sirve para descargar imagenes de gettyImages en conjunto de la extenxión
-'''
-
 #Ingresamos la palabra a buscar para generar los links de imagenes a descargar
 searching = input("Ingresa la palabra a buscar en getty Images:\n")
 cantidad = int(input("Ingresa la cantidad de páginas a scrappear (se recomienda 10)\n"))
