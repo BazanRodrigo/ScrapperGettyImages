c7f1e55a6f12ddbf95097ad1acc98f660e0b2963
Rodrigo Bazán Zurita <bazanzuritarodrigo@gmail.com>
2023-05-11 16:49:31 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Scrapper para gettyImages
+# Scrapper para gettyImages
 Este código funciona en complemento de la extensión:
 > Fatkun Batch Descargar imagen.  
   
@@ -13,7 +13,18 @@ La extensíon se puede descargar en la tienda de extensiones de chrome, para pod
 6.- Copiar todos los links y pegarlos en el archivo **_url.txt_**  
 7.- Ejecutar el archivo **_downloader.py_**
 
-El script filtrador.py es para filtrar las imagenes funcionales y las no funcionales, si te sirve la imagen, selecciona la paloma de color verde, si no, selecciona la paloma de color rojo.  
-Al final el script te devuelve una carpeta llamada Imagenes filtradas y listas para proceder a etiquetar las imagenes  
+El script filtrador.py es para filtrar las imagenes funcionales y las no funcionales, si te sirve la imagen, selecciona la paloma de color verde, si no, selecciona la equis de color rojo; funciona igual con teclas hacia arriba y hacia abajo respectivamente.
+Al final el script te devuelve una carpeta llamada Imagenes filtradas, las cuales están listas para proceder a etiquetar las imagenes usando LabelImg.py
 
 Una vez hayas etiquetado las imagenes, puedes ejecutar el script  OrdenadorNumerador.py, para que todas las imagenes tengan una numeración cotinua sin perder las etiquetas generadas para entrenar YOLO
+
+
+# Etiquetación de imagenes
+https://github.com/heartexlabs/labelImg
+1. Corre el código 
+**pip install pyinstaller**
+**conda install pyqt=5**
+**conda install -c anaconda lxml**
+**pyrcc5 -o libs/resources.py resources.qrc**
+**python labelImg.py**
+
--- filtrador.py
+++ filtrador.py
@@ -1,119 +1,99 @@
-import pygame
 import os
 import shutil
-from pygame.locals import *
+import pygame
+import glob
 
-img = 1
-listaImg = []
-
-
-def save(img):
-    destino = 'ImagenesFiltradas/' + str(img[img.index("/"):])
+def save(carpeta,img):
+    """Guarda la imagen en una carpeta llamada ImagenesFiltradas."""    
+    destino = carpeta+'/'+'ImagenesFiltradas/' + os.path.basename(img)
     shutil.copy(img, destino)
 
-
-def ordenador(listaArchivos, name):
+def ordenador(lista_archivos, name):
+    """Ordena los archivos en una carpeta."""
+    os.chdir('ImagenesFiltradas/')
     i = 0
-    listaArchivos.sort()
-    for archivo in listaArchivos:
-        if i < 10:
-            os.rename(archivo, name + '000' + str(i) + '.jpg')
-        if i >= 10 and i < 100:
-            os.rename(archivo, name + '00' + str(i) + '.jpg')
-        if i >= 100 and i < 1000:
-            os.rename(archivo, name + '0' + str(i) + '.jpg')
-        if i >= 1000 :
-            os.rename(archivo, name + str(i) + '.jpg')
+    lista_archivos.sort()
+    for archivo in lista_archivos:
+        nombre, extension = os.path.splitext(archivo)
+        nombre = name + f'{i:04d}'
+        os.rename(archivo, nombre + extension)
         i += 1
+
+def lastImage():
+    # Obtener la lista de imágenes con cualquier extensión en la carpeta ImagenesFiltradas    
+    imagenes = glob.glob(os.path.join("ImagenesFiltradas", "*.*"))
+    if not imagenes:        
+        return -1
+    # Ordenar la lista según el número que se encuentra antes de la extensión
+    imagenes_ordenadas = sorted(imagenes, key=lambda x: int(os.path.splitext(x)[0][-4:]))
+    # Obtener la imagen con la terminación más grande
+    imagen_mas_grande = imagenes_ordenadas[-1]
+    # Cargar la imagen y devolverla
+    return imagen_mas_grande[18:]
+
 
 def main():
-    name = input("Ingresa el nombre de la carpeta con las imagenes\n")
-    try:
-        os.chdir(name + '/')
-        listaImg = os.listdir()
-        listaImg.sort()
-    except FileNotFoundError :
-        name = input("No se encuentra la carpeta\nIngresa el nombre de la carpeta con las imagenes\n")
-        os.chdir(name + '/')
-        listaImg = os.listdir()
-        listaImg.sort()
-    os.chdir('../')
-    #Ordenando las imagenes filtradas
-    print(listaImg)
-    ordenador(listaImg, name)
-    try:
-        os.chdir('ImagenesFiltradas/')
-    except FileNotFoundError :
-        os.mkdir('ImagenesFiltradas/')
-
-    #ordenador(os.listdir(), name)
-    try:
+    """Función principal que ejecuta el programa."""
+    # Se pide al usuario el nombre de la carpeta con las imágenes.
+    while True:
+        nombre_carpeta = input("Ingresa el nombre de la carpeta con las imágenes: ")
+        if os.path.isdir(nombre_carpeta):
+            break
+        print("No se encuentra la carpeta.")
+    os.chdir(nombre_carpeta)
+    if not os.path.isdir('ImagenesFiltradas'):
         os.mkdir('ImagenesFiltradas')
-        os.chdir('../')
-    except FileExistsError:
-        os.chdir('../')
-        print("\n")
-
-    i = 0
-    for imagen in listaImg:
-        listaImg[i] = name + '/' + str(listaImg[i])
-        i += 1
-    wp = pygame.image.load('DataProgram/wallpaperPrincipal.jpg')
-    correctimg = pygame.image.load('Icons/correct.png')
-    incorrectimg = pygame.image.load('Icons/incorrect.png')
+    # Se cargan las imágenes y se muestran en la pantalla.
+    lista_img = [os.path.join(nombre_carpeta, f) for f in os.listdir() if f.endswith(('.jpg', '.jpeg', '.png'))]
+    lista_img.sort()
+    li = lastImage()
+    print(li,'*'*500)
+    print(lista_img)
+    if li != -1:
+        imgg = nombre_carpeta + '\\' + li        
+        lista_img = lista_img[lista_img.index(imgg):]
     pygame.init()
-    pygame.display.init()
-    pygame.display.set_caption('Filtrar imagenes')
-    window = pygame.display.set_mode([1000, 720])
-    window.blit(wp, (0, 0))
-    window.blit(correctimg, (800, 100))
-    window.blit(incorrectimg, (800, 250))
-    pygame.display.flip()
-    window.blit(pygame.image.load(listaImg[0]), (50, 50))
-    pygame.display.flip()
-    running = True
-    listaImg.sort()
-    imagen = 0
+    pygame.display.set_caption('Filtrar imágenes')
+    pantalla = pygame.display.set_mode([1000, 720])
+    os.chdir('../')
+    wp = pygame.image.load('wallpaperPrincipal.jpg')
+    correct_img = pygame.image.load('Icons/correct.png')
+    incorrect_img = pygame.image.load('Icons/incorrect.png')
     i = 0
-    while running:
-        # Did the user click the window close button?
+    while i < len(lista_img):
+        pantalla.blit(wp, (0, 0))
+        pantalla.blit(correct_img, (800, 100))
+        pantalla.blit(incorrect_img, (800, 250))
+        imagen = pygame.image.load(lista_img[i])
+        pantalla.blit(imagen, (50, 50))
+        pygame.display.flip()
+
+        # Se esperan eventos del usuario.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
+                return
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    save(listaImg[i])
-                    i += 1
-                    print("Se guardo " + str(listaImg[i]))
-                if event.key == pygame.K_DOWN:
-                    print("No se guardo " + str(listaImg[i]))
-                    i += 1
-
+                    save(nombre_carpeta ,lista_img[i])
+                    print(f"Se guardó {lista_img[i]}")
+                elif event.key == pygame.K_DOWN:
+                    print(f"No se guardó {lista_img[i]}")
+                i += 1
             if event.type == pygame.MOUSEBUTTONDOWN:
-                x = pygame.mouse.get_pos()[0]
-                y = pygame.mouse.get_pos()[1]
-                if x > 800 and x < 928 and y > 100 and y < 225:
-                    save(listaImg[i])
+                x, y = pygame.mouse.get_pos()
+                if 800 < x < 928 and 100 < y < 225:
+                    save(nombre_carpeta ,lista_img[i])
+                    print(f"Se guardó {lista_img[i]}")
                     i += 1
-                    print("Se guardo " + str(listaImg[i]))
-                if x > 800 and x < 928 and y > 250 and y < 375:
-                    print("No se guardo " + str(listaImg[i]))
+                elif 800 < x < 928 and 250 < y < 375:
+                    print(f"No se guardó {lista_img[i]}")
                     i += 1
-            window.blit(wp, (0, 0))
-            window.blit(correctimg, (800, 100))
-            window.blit(incorrectimg, (800, 250))
-            try:
-                window.blit(pygame.image.load(listaImg[i]), (50, 50))
-            except:
-                imagen += 1
-                window.blit(pygame.image.load(listaImg[i]), (50, 50))
-            pygame.display.flip()
-            if i-1 == len(listaImg):
-                running = False
-    #Ordenando las imagenes filtradas
-    os.chdir('ImagenesFiltradas/')
-    ordenador(os.listdir(), name)
 
+    # Se ordenan las imágenes guardadas.
+    os.chdir('..')
+    os.makedirs('ImagenesFiltradas', exist_ok=True)
+    ordenador(os.listdir(nombre_carpeta), nombre_carpeta)
 
 if __name__ == '__main__':
     main()
